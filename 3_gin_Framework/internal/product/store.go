d6353a7eea11bfdf6d98c7be3a2dbd69c9f61e62
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-01 20:18:58 +0600

    memory data storage

---
+++ 3_gin_Framework/internal/product/store.go
@@ -0,0 +1,11 @@
+package product
+
+var products=[] Product{
+	{Name:"Apple",Price: 10},
+	{Name: "Mango",Price: 20},
+	{Name: "Jack Fruit",Price: 100},
+}
+
+func List() []Product{
+	return products
+}
