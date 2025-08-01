aec7ca83ab9086332e986098c32fa0e84fd9e6ab
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-01 20:18:14 +0600

    entry point

---
+++ 3_gin_Framework/cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/alamgir-ahosain/go-rest-api-3ways/3_gin_Framework/internal/product"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+
+
+func main(){
+	
+	router:=gin.Default()
+	product.RegisterRoutes(router)
+	err :=router.Run("localhost:8080")
+	if err!=nil {
+		panic("Failed to start server"+err.Error())
+	}
+
+}
